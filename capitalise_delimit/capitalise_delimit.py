9d7ab791221f1a35734acd6c40ba80c41577c751
jkoninger <jck999992@gmail.com>
2022-08-13 00:43:16 +0100

    Class changed to take 'delimiters' upon initialisation and 'string' upon each invocation of .capitalise()

--- capitalise_delimit/capitalise_delimit.py
+++ capitalise_delimit/capitalise_delimit.py
@@ -1,20 +1,37 @@
-class CapitaliseDelimit:
-    def __init__(self, string: str):
-        self.string = string
-        self.capitalised = ""
+from typing import Union
 
-    def capitalise(self, delimiters: list):
+
+class CapitaliseDelimit:
+
+    def __init__(self, delimiters: Union[list[str], set[str]]):
+        # self.delimiters converted to set to ensure uniqueness of elements and back to list for use
+        self.delimiters = self.__make_set_of_strings(delimiters, 'delimiters')
+        self.delimiters = list(delimiters)
+        # list of popular syncategorematic words that may not always want to be capitalised
+        self.words_to_ignore = {'upon', 'at', 'the', 'on', 'in'}
+
+    def capitalise(self, string: str, ignore_words: bool = True,
+                   custom_ignore_words: Union[list[str], set[str]] = None):
         """
         'do_capitalise' called separately as it is a recursive function and the parameter 'string' it takes in is
         modified upon each invocation
-        :param delimiters: list of delimiters to split and capitalise by
+        :param string: the string to capitalise
+        :param ignore_words: boolean specifying if to ignore (i.e. don't capitalise) the defined list
+        of syncategorematic words
+        :param custom_ignore_words: custom set of words to ignore during capitalisation. Can be used in conjunction
+        with or instead of the predefined set of words
         :return: capitalised form of string
         """
-        self.capitalised = self.__do_capitalise(self.string, delimiters)
-        return self.capitalised
+        if not isinstance(string, str):
+            raise TypeError(f"Please provide a string to be capitalised ({type(string)} provided)")
+
+        if custom_ignore_words:
+            custom_ignore_words = self.__make_set_of_strings(custom_ignore_words, 'custom ignore words')
+            self.words_to_ignore.update(custom_ignore_words)
+        return self.__do_capitalise(string, self.delimiters, self.words_to_ignore)
 
     @staticmethod
-    def __do_capitalise(string, delimiters: list):
+    def __do_capitalise(string, delimiters: set[str], words_to_ignore: set[str]):
         """
         Recursively split string by given delimiters, then capitalise individual parts before joining them back together
         with the delimiter that the string was split by
@@ -23,13 +40,24 @@ class CapitaliseDelimit:
         :return: capitalised string
         """
         if len(delimiters) == 0:
-            if len(string) > 1:
-                string = string.capitalize()
-            else:
-                string = string.capitalize()
+            if string not in words_to_ignore:
+                if len(string) > 1:
+                    string = string.capitalize()
+                else:
+                    string = string.capitalize()
         else:
             string = string.split(delimiters[0])
             for s in range(len(string)):
-                string[s] = CapitaliseDelimit.__do_capitalise(string[s], delimiters[1:])
+                string[s] = CapitaliseDelimit.__do_capitalise(string[s], delimiters[1:], words_to_ignore)
             string = f'{delimiters[0]}'.join(string)
         return string
+
+    @staticmethod
+    def __make_set_of_strings(items, param_name):
+        try:
+            items = set(items)
+            if not all(isinstance(item, str) for item in items):
+                raise TypeError
+        except TypeError:
+            raise TypeError(f"Please ensure the parameter provided for {param_name} is a list/set of strings only")
+        return items
--- tests/capitalise_delimit_tests.py
+++ tests/capitalise_delimit_tests.py
@@ -5,31 +5,32 @@ from capitalise_delimit import CapitaliseDelimit
 class CapitaliseDelimitTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.string = CapitaliseDelimit('some-sequence of@random~words')
+        self.no_delimiters = CapitaliseDelimit([])
+        self.test_string = 'some-sequence of@random~words'
 
     def test_single_word_no_delimiters(self):
-        self.assertEqual(CapitaliseDelimit('string').capitalise([]), 'String')
+        self.assertEqual(self.no_delimiters.capitalise('string'), 'String')
 
     def test_multiple_words_no_delimiters(self):
-        self.assertEqual(self.string.capitalise([]), 'Some-sequence of@random~words')
+        self.assertEqual(self.no_delimiters.capitalise(self.test_string), 'Some-sequence of@random~words')
 
     def test_empty_string_no_delimiters(self):
-        self.assertEqual(CapitaliseDelimit('').capitalise([]), '')
+        self.assertEqual(self.no_delimiters.capitalise(''), '')
 
     def test_empty_string_with_delimiters(self):
-        self.assertEqual(CapitaliseDelimit('').capitalise([' ', '@']), '')
+        self.assertEqual(CapitaliseDelimit([' ', '@']).capitalise(''), '')
 
     def test_single_word_with_delimiters(self):
-        self.assertEqual(CapitaliseDelimit('string').capitalise(['-', ' ', '@', '~']), 'String')
+        self.assertEqual(CapitaliseDelimit(['-', ' ', '@', '~']).capitalise('string'), 'String')
 
     def test_multiple_words_with_some_delimiters(self):
-        self.assertEqual(self.string.capitalise(['-', '@']), 'Some-Sequence of@Random~words')
+        self.assertEqual(CapitaliseDelimit(['-', '@']).capitalise(self.test_string), 'Some-Sequence of@Random~words')
 
     def test_multiple_words_with_all_relevant_delimiters(self):
-        self.assertEqual(self.string.capitalise(['-', ' ', '@', '~']), 'Some-Sequence Of@Random~Words')
+        self.assertEqual(CapitaliseDelimit(['-', ' ', '@', '~']).capitalise(self.test_string), 'Some-Sequence Of@Random~Words')
 
     def test_multiple_words_with_extra_delimiters(self):
-        self.assertEqual(self.string.capitalise(['-', ' ', '@', '~', '?', '/']), 'Some-Sequence Of@Random~Words')
+        self.assertEqual(CapitaliseDelimit(['-', ' ', '@', '~', '?', '/']).capitalise(self.test_string), 'Some-Sequence Of@Random~Words')
 
 
 if __name__ == '__main__':
